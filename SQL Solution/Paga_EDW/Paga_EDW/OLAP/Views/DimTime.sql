74fcc2f00a02685ff42caf91b4506cb19178afb0
christinaleo <christina.leo@aphelion.bi>
2014-08-26 20:58:20 +0200

    Updated DimTime view

--- SQL Solution/Paga_EDW/Paga_EDW/OLAP/Views/DimTime.sql
+++ SQL Solution/Paga_EDW/Paga_EDW/OLAP/Views/DimTime.sql
@@ -1,72 +1,12 @@
-﻿
+﻿CREATE VIEW OLAP.DimTime AS
+SELECT [DimTimeID]
+      ,[FullTime]
+      ,[HourNumber]
+      ,[MinuteNumber]
+      ,[SecondNumber]
+      ,[TimeInSecond]
+      ,[HourlyBucket]
+      ,[DayTimeBucketGroupKey]
+      ,[DayTimeBucket]
+  FROM [Shared].[DimTime]
 GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
-
-GO
-
-
