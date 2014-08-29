08dea56441ffe069c9afac25026d837b4ba979e7
christinaleo <christina.leo@aphelion.bi>
2014-08-29 16:49:18 +0200

    Refactored COA Separeated Merchants and Dealers

---
+++ SQL Solution/Paga_EDW/Paga_EDW/Shared/Tables/DimMerchantPagaAccount.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE [Shared].[DimMerchantPagaAccount] (
+    [DimMerchantPagaAccountID]         INT           NOT NULL,
+    [SourceKey]                VARCHAR (255) NOT NULL,
+    [Name]                     VARCHAR (255) NOT NULL,
+    [Code]                     VARCHAR (50)  NOT NULL,
+    [DimPagaAccountStatusID]   INT           NOT NULL,
+    [DimDealerID]                INT           NOT NULL,
+	[DimPagaAccountUserTypeID] INT			 NOT NULL,
+    [RegistrationDateID]       INT           NOT NULL,
+    [PagaAccountNumber]        VARCHAR (12)  NULL,
+    [ExternalAccountNumber]    VARCHAR (12)  NULL,
+    [BankingStatus]            VARCHAR (50)  NOT NULL,
+    [hasOnlineAccount]         BIT           NULL,
+    [IsActive]                 BIT           NULL,
+    [IsEnabled]                BIT           NULL,
+    [SourceKeyHash]            BIGINT        NOT NULL,
+    [DeltaHash]                BIGINT        NOT NULL,
+    [sys_ModifiedBy]           VARCHAR (255) DEFAULT (suser_sname()) NOT NULL,
+    [sys_ModifiedOn]           DATETIME      DEFAULT (getdate()) NOT NULL,
+    [sys_CreatedBy]            VARCHAR (255) DEFAULT (suser_sname()) NOT NULL,
+    [sys_CreatedOn]            DATETIME      DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [pk_DimMerchantPagaAccountID] PRIMARY KEY CLUSTERED ([DimMerchantPagaAccountID] ASC),
+    CONSTRAINT [fk_DimMerchantPagaAccount_DimPagaAccountStatusID] FOREIGN KEY ([DimPagaAccountStatusID]) REFERENCES [Classification].[DimPagaAccountStatus] ([DimPagaAccountStatusID]),
+    CONSTRAINT [fk_DimMerchantPagaAccount_DimDealerID] FOREIGN KEY ([DimDealerID]) REFERENCES [Shared].dimDealer (DimDealerID),
+    CONSTRAINT [fk_DimMerchantPagaAccount_RegistrationDateID] FOREIGN KEY ([RegistrationDateID]) REFERENCES [Shared].[DimDate] ([DimDateID]),
+	CONSTRAINT [fk_DimMerchantPagaAccount_DimPagaAccountUserTypeID] FOREIGN KEY (DimPagaAccountUserTypeID) REFERENCES Classification.dimPagaaccountUserType (DimPagaAccountUserTYpeID)
+);
+
+
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [ix_DimMerchantPagaAccount_SourceKey]
+    ON [Shared].[DimMerchantPagaAccount]([SourceKey] ASC);
+
+
