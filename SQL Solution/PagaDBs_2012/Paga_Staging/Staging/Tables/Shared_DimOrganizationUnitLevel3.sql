6d7fa9bb953bfc3fe7c48f059c28c617e93342ff
christinaleo <christina.leo@aphelion.bi>
2014-09-11 13:30:25 +0200

    Removed DimUserSourceKey from OrganizationUnit Staging Tables

--- SQL Solution/PagaDBs_2012/Paga_Staging/Staging/Tables/Shared_DimOrganizationUnitLevel3.sql
+++ SQL Solution/PagaDBs_2012/Paga_Staging/Staging/Tables/Shared_DimOrganizationUnitLevel3.sql
@@ -2,7 +2,6 @@
     [IdentificationNumber]               VARCHAR (20)  NULL,
     [Name]                               VARCHAR (255) NOT NULL,
     [SourceKey]                          VARCHAR (255) NOT NULL,
-    [DimUserSourceKey]                 VARCHAR (255) NOT NULL,
     [DimOrganizationUnitLevel2SourceKey] VARCHAR (255) NOT NULL,
     [DimOrganizationUnitTypeSourceKey]   VARCHAR (255) NOT NULL,
     [change_operation]                   CHAR (1)      DEFAULT ((1)) NOT NULL
