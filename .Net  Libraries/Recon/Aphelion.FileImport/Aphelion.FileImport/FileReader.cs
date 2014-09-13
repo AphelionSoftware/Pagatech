b4dd49e37dd248befd2bfe973439a2843575d276
MarkGStacey <MarkGStacey@aphelion.bi>
2014-09-13 16:16:27 +0000

    Frame

---
+++ .Net  Libraries/Recon/Aphelion.FileImport/Aphelion.FileImport/FileReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.VisualBasic.FileIO;
+using System.ComponentModel;
+/*// use "Microsoft.VisualBasic.dll"
+
+using System;
+using Microsoft.VisualBasic.FileIO;
+
+class Program {
+    static void Main(string[] args){
+        using(var csvReader = new TextFieldParser(@"sportsResults.csv")){
+            csvReader.SetDelimiters(new string[] {","});
+            string [] fields;
+            while(!csvReader.EndOfData){
+                fields = csvReader.ReadFields();
+                Console.WriteLine(String.Join(",",fields));//replace make instance
+            }
+        }
+    }
+}*/
+namespace Aphelion.FileImport
+{
+    public class FileReader
+    {
+        public string sFileName { get; set; }
+        public DataTable dtResults { get; }
+
+        public int iStartRow = 0;
+        public int iNumRows = 0;
+
+        public FileReader()
+        {
+            
+        }
+    }
+}
