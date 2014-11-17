044fbe754fb61ac4e8a2ad4e760d7584fd1857c1
christinaleo <christina.leo@aphelion.bi>
2014-11-17 19:05:56 +0000

    NIBBS Staging Tables Added

---
+++ SQL Solution/Aphelion.PagaRecon.DB/Aphelion.PagaRecon.DB/Staging/Tables/NIBBS_GL_Detail.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [Staging].[NIBBS_GL_Detail] (
+    [FinancialTransactionId]     INT             NOT NULL,
+    [FinancialTransactionItemId] INT             NOT NULL,
+    [FinancialTransactionTypeId] VARCHAR (50)    NULL,
+    [Amount]                     DECIMAL (18, 2) NULL,
+    [ExternalReferenceNumber]    NVARCHAR (50)   NULL,
+    [Fee]                        DECIMAL (18, 2) NULL,
+    [ShortCode]                  VARCHAR (10)    NULL,
+    [TransactionDate]            DATETIME        NULL,
+    [ReferenceNumber]            VARCHAR (50)    NULL,
+    [AccountCode]                INT             NULL,
+    [CreditAmount]               DECIMAL (18, 2) NULL,
+    [DebitAmount]                DECIMAL (18, 2) NULL,
+    [FinancialAccountTypeId]     VARCHAR (70)    NULL,
+    [Cancelled]                  INT             NULL,
+    [__Reverses]                 INT             NULL,
+    [__ReversedBy]               INT             NULL,
+    [AccountHolderId]            INT             NULL,
+    [BankAccountId]              INT             NULL,
+    [UserId]                     INT             NULL,
+    [TransactionLineNumber]      INT             NULL
+);
+
