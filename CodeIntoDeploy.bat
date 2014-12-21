45a87a797bc89655bafb72d681524e46117ac6f2
christinaleo <christina.leo@aphelion.bi>
2014-12-21 19:03:09 +0000

    Batch changes

--- CodeIntoDeploy.bat
+++ CodeIntoDeploy.bat
@@ -1,8 +1,8 @@
 "C:\Program Files (x86)\Git\bin\git.exe" fetch --all
 "C:\Program Files (x86)\Git\bin\git.exe" pull --rebase --progress "origin" +refs/heads/r02_Joint/Deployment:+refs/heads/r02_Joint/Deployment:refs/remotes/origin/r02_Joint/Deployment
 "C:\Program Files (x86)\Git\bin\git.exe" checkout --force "r02_Joint/Deployment"
-"C:\Program Files (x86)\Git\bin\git.exe" pull --rebase --progress "origin" +refs/heads/r02_Joint/Deployment:refs/remotes/origin/r02_Joint/Development
-"C:\Program Files (x86)\Git\bin\git.exe" merge r02_Joint/Development
+"C:\Program Files (x86)\Git\bin\git.exe" pull --rebase --progress "origin" +refs/heads/r02_Joint/Deployment:refs/remotes/origin/r01_Finance/Development
+"C:\Program Files (x86)\Git\bin\git.exe" merge r01_Finance/Development
 "C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r02_Joint/Deployment:refs/heads/r02_Joint/Deployment
-"C:\Program Files (x86)\Git\bin\git.exe" checkout --force "r02_Joint/Development"
-"C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r02_Joint/Development:refs/heads/r02_Joint/Development
+"C:\Program Files (x86)\Git\bin\git.exe" checkout --force "r01_Finance/Development"
+"C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r01_Finance/Development:refs/heads/r01_Finance/Development
