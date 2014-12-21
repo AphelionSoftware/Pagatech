f26d784983c2354aa0278da5e6ec38e800d16851
christinaleo <christina.leo@aphelion.bi>
2014-12-21 18:29:30 +0000

    DeployDB and BuildDB Updates

---
+++ SQL Solution/Aphelion.PagaWarehouse.DB/Aphelion.DB.EDW/Paga_EDW/Mapping/Tables/PaymentUseCaseToTxTypeMap.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Mapping].[PaymentUseCaseToTxTypeMap] (
+    [PaymentUseCaseToTxTypeMapID] INT           IDENTITY (1, 1) NOT NULL,
+    [DimPaymentUseCaseID]         INT           NOT NULL,
+    [DimFinancialTxTypeID]        INT           NOT NULL,
+    [FinancialTxTypeName]         VARCHAR (255) NULL,
+    [sys_ModifiedBy]              VARCHAR (255) DEFAULT (suser_sname()) NOT NULL,
+    [sys_ModifiedOn]              DATETIME      DEFAULT (getdate()) NOT NULL,
+    [sys_CreatedBy]               VARCHAR (255) DEFAULT (suser_sname()) NOT NULL,
+    [sys_CreatedOn]               DATETIME      DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [pk_DimPaymentUseCaseID] PRIMARY KEY CLUSTERED ([PaymentUseCaseToTxTypeMapID] ASC),
+    CONSTRAINT [FK_PaymentUseCaseToTxTypeMap_DimFinancialTxTypeID] FOREIGN KEY ([DimFinancialTxTypeID]) REFERENCES [Classification].[DimFinancialTransactionType] ([DimFinancialTxTypeID]),
+    CONSTRAINT [FK_PaymentUseCaseToTxTypeMap_DimPaymentUseCaseID] FOREIGN KEY ([DimPaymentUseCaseID]) REFERENCES [Finance].[DimPaymentUseCase] ([DimPaymentUseCaseID])
+);
+
