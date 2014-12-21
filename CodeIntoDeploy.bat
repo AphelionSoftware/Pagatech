1542fe6a74442cd3a63836c0d61cee890bd7e295
christinaleo <christina.leo@aphelion.bi>
2014-12-21 19:08:54 +0000

    CIN TOD

--- CodeIntoDeploy.bat
+++ CodeIntoDeploy.bat
@@ -5,4 +5,4 @@
 "C:\Program Files (x86)\Git\bin\git.exe" merge r01_Finance/Development
 "C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r02_Joint/Deployment:refs/heads/r02_Joint/Deployment
 "C:\Program Files (x86)\Git\bin\git.exe" checkout --force "r01_Finance/Development"
-"C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r01_Finance/Development:refs/heads/r01_Finance/Development
+"C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r01_Finance/Development:refs/remotes/origin/r01_Finance/Development
