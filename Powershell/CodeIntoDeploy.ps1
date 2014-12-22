d1fbe9a2e88fd9a5781a40215b82de949b46fb49
christinaleo <christina.leo@aphelion.bi>
2014-12-22 16:46:20 +0000

    Migrated Changes from Meerkat deployment testing

--- Powershell/CodeIntoDeploy.ps1
+++ Powershell/CodeIntoDeploy.ps1
@@ -1,9 +1,35 @@
-﻿git fetch --all
-git pull --rebase --progress "origin" +refs/heads/r02_Joint/Deployment:+refs/heads/r02_Joint/Deployment:refs/remotes/origin/r02_Joint/Deployment
-git checkout --force "r02_Joint/Deployment"
-git pull --rebase --progress "origin" +refs/heads/r02_Joint/Deployment:refs/remotes/origin/r01_Finance/Development
-git merge r01_Finance/Development
-git push --progress "origin" refs/heads/r02_Joint/Deployment:refs/heads/r02_Joint/Deployment
-git checkout --force "r01_Finance/Development"
-git push --progress "origin" refs/heads/r01_Finance/Development:refs/remotes/origin/r01_Finance/Development
-git push --recurse-submodules=check --progress "origin" refs/heads/R01_Finance/Development:refs/heads/R01_Finance/Development
+﻿<#
+$GitFolder = "E:\Github\Pagatech"
+$BranchPrefix = "R02_Joint"
+#>
+
+Param (
+    [Parameter(Mandatory=$True)][String]$GitFolder,
+    [Parameter(Mandatory=$True)][String]$BranchPrefix
+      )
+
+cd $GitFolder
+
+
+$DepBranch = "refs/heads/" +$BranchPrefix +"/Deployment"
+$DevBranch = "refs/heads/" +$BranchPrefix +"/Development"
+
+$DepToDep = $DepBranch +":" +$DepBranch
+$DevToDev = $DevBranch +":" +$DevBranch
+$DepToDev = $DepBranch +":" +$DevBranch
+
+
+
+
+
+git fetch --all
+git pull --rebase --progress "origin"$DepToDep
+git checkout --force $BranchPrefix"/Deployment"
+git pull --rebase --progress "origin"$DepToDev
+git merge $DevBranch
+git push --progress "origin" $DepToDep
+git checkout --force $BranchPrefix"/Development"
+git push --progress "origin"$DepToDev 
+git push --recurse-submodules=check --progress "origin" $DepToDep
+git push --recurse-submodules=check --progress "origin" $DevToDev
+
