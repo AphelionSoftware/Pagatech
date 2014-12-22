e77d4ebe936077f11d2ff7198e0ea6dbd8999cf3
christinaleo <christina.leo@aphelion.bi>
2014-12-22 15:36:37 +0000

    CodeIntoDeploy.ps1

---
+++ Powershell/CodeIntoDeploy.ps1
@@ -0,0 +1,8 @@
+﻿git fetch --all
+git pull --rebase --progress "origin" +refs/heads/r02_Joint/Deployment:+refs/heads/r02_Joint/Deployment:refs/remotes/origin/r02_Joint/Deployment
+git checkout --force "r02_Joint/Deployment"
+git pull --rebase --progress "origin" +refs/heads/r02_Joint/Deployment:refs/remotes/origin/r01_Finance/Development
+git merge r01_Finance/Development
+git push --progress "origin" refs/heads/r02_Joint/Deployment:refs/heads/r02_Joint/Deployment
+git checkout --force "r01_Finance/Development"
+git push --progress "origin" refs/heads/r01_Finance/Development:refs/remotes/origin/r01_Finance/Development
