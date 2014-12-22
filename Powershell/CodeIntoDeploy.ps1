79582f99084edfad757e06a6fbc183281370a5df
christinaleo <christina.leo@aphelion.bi>
2014-12-22 15:38:23 +0000

    added one more push to bring all branches to the same node

--- Powershell/CodeIntoDeploy.ps1
+++ Powershell/CodeIntoDeploy.ps1
@@ -6,3 +6,4 @@ git merge r01_Finance/Development
 git push --progress "origin" refs/heads/r02_Joint/Deployment:refs/heads/r02_Joint/Deployment
 git checkout --force "r01_Finance/Development"
 git push --progress "origin" refs/heads/r01_Finance/Development:refs/remotes/origin/r01_Finance/Development
+git push --recurse-submodules=check --progress "origin" refs/heads/R01_Finance/Development:refs/heads/R01_Finance/Development
