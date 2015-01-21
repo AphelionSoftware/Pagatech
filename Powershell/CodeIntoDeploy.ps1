353b43e2e5a1313ff6b27c872e8333750ace31fe
christinaleo <christina.leo@aphelion.bi>
2015-01-21 09:59:55 +0100

    Added more progress messages

--- Powershell/CodeIntoDeploy.ps1
+++ Powershell/CodeIntoDeploy.ps1
@@ -13,17 +13,46 @@ $DevToDev = $DevBranch +":" +$DevBranch
 $DepToDev = $DepBranch +":" +$DevBranch
 
 
-
+cls
 
 
 git fetch --all 2>&1 | %{ "$_" } 
-git pull --rebase --progress "origin"$DepToDep 2>&1 | %{ "$_" } 
-git checkout --force $BranchPrefix"/Deployment" 2>&1 | %{ "$_" } 
+Echo ""
+
+Echo "Pull Origin" $DepToDep
+git pull --rebase --progress "origin"$DepToDep 2>&1 | %{ "$_" }
+Echo "" 
+
+Echo "Checkout " $BranchPrefix"/Deployment"
+git checkout --force $BranchPrefix"/Deployment" 2>&1 | %{ "$_" }
+Echo "" 
+
+Echo "Pull origin"$DepToDev
 git pull --rebase --progress "origin"$DepToDev 2>&1 | %{ "$_" } 
+Echo ""
+
+Echo "merge " $DevBranch
 git merge $DevBranch 2>&1 | %{ "$_" } 
+Echo ""
+
+Echo "Push origin" $DepToDep
 git push --progress "origin" $DepToDep 2>&1 | %{ "$_" } 
+Echo ""
+
+Echo "checkout " $BranchPrefix"/Development"
 git checkout --force $BranchPrefix"/Development" 2>&1 | %{ "$_" } 
-git push --progress "origin"$DepToDev  2>&1 | %{ "$_" } 
+Echo ""
+
+Echo "Push origin"$DepToDev 
+git push --progress "origin"$DepToDev  2>&1 | %{ "$_" }
+Echo "" 
+
+Echo  "Push origin" $DepToDep
 git push --recurse-submodules=check --progress "origin" $DepToDep 2>&1 | %{ "$_" } 
+Echo ""
+
+
+Echo  "Push origin" $DevToDev
 git push --recurse-submodules=check --progress "origin" $DevToDev 2>&1 | %{ "$_" } 
+Echo ""
 
