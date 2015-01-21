2a3e13c94c3f702acdcd75b6802b6f6921849a03
christinaleo <christina.leo@aphelion.bi>
2015-01-21 09:52:44 +0100

    Added error redirect to supress FullyQualifiedErrorId : NativeCommandError messages

--- Powershell/CodeIntoDeploy.ps1
+++ Powershell/CodeIntoDeploy.ps1
@@ -1,12 +1,6 @@
-﻿<#
+﻿
 $GitFolder = "E:\Github\Pagatech"
 $BranchPrefix = "R02_Joint"
-#>
-
-Param (
-    [Parameter(Mandatory=$True)][String]$GitFolder,
-    [Parameter(Mandatory=$True)][String]$BranchPrefix
-      )
 
 cd $GitFolder
 
@@ -22,14 +16,14 @@ $DepToDev = $DepBranch +":" +$DevBranch
 
 
 
-git fetch --all
-git pull --rebase --progress "origin"$DepToDep
-git checkout --force $BranchPrefix"/Deployment"
-git pull --rebase --progress "origin"$DepToDev
-git merge $DevBranch
-git push --progress "origin" $DepToDep
-git checkout --force $BranchPrefix"/Development"
-git push --progress "origin"$DepToDev 
-git push --recurse-submodules=check --progress "origin" $DepToDep
-git push --recurse-submodules=check --progress "origin" $DevToDev
+git fetch --all 2>&1 | %{ "$_" } 
+git pull --rebase --progress "origin"$DepToDep 2>&1 | %{ "$_" } 
+git checkout --force $BranchPrefix"/Deployment" 2>&1 | %{ "$_" } 
+git pull --rebase --progress "origin"$DepToDev 2>&1 | %{ "$_" } 
+git merge $DevBranch 2>&1 | %{ "$_" } 
+git push --progress "origin" $DepToDep 2>&1 | %{ "$_" } 
+git checkout --force $BranchPrefix"/Development" 2>&1 | %{ "$_" } 
+git push --progress "origin"$DepToDev  2>&1 | %{ "$_" } 
+git push --recurse-submodules=check --progress "origin" $DepToDep 2>&1 | %{ "$_" } 
+git push --recurse-submodules=check --progress "origin" $DevToDev 2>&1 | %{ "$_" } 
 
