5a9be9e8fab72666dbd7df647d3ab6348c0e9d84
christinaleo <christina.leo@aphelion.bi>
2015-01-25 12:28:30 +0100

    More Base Query Corrections

--- SQL Solution/Aphelion.PagaWarehouse.DB/Aphelion.DB.EDW/Paga_EDW/Shared/Tables/Dim_User.sql
+++ SQL Solution/Aphelion.PagaWarehouse.DB/Aphelion.DB.EDW/Paga_EDW/Shared/Tables/Dim_User.sql
@@ -1,26 +1,27 @@
 ﻿CREATE TABLE [Shared].[DimUser] (
-    [DimUserID]      INT             IDENTITY (1, 1) NOT NULL,
-    [SourceKey]      INT             NOT NULL,
-    [Name]           VARCHAR (255)   NOT NULL,
-    [DateOfBirthID]  INT             NULL,
-    [FirstName]      VARCHAR (255)   NULL,
-    [MiddleName]     VARCHAR (255)   NULL,
-    [LastName]       VARCHAR (255)   NULL,
-    [Gender]         VARCHAR (50)    NULL,
-    [PhoneNumber]    VARBINARY (256) NULL,
-    [Email]          VARCHAR (100)   NULL,
-    [IsEnabled]      BIT             NULL,
-    [CreatedDateID]  INT             NOT NULL,
-    [SourceKeyHash]  BIGINT          NULL,
-    [DeltaHash]      BIGINT          NULL,
-    [sys_ModifiedBy] VARCHAR (255)   DEFAULT (suser_sname()) NOT NULL,
-    [sys_ModifiedOn] DATETIME        DEFAULT (getdate()) NOT NULL,
-    [sys_CreatedBy]  VARCHAR (255)   DEFAULT (suser_sname()) NOT NULL,
-    [sys_CreatedOn]  DATETIME        DEFAULT (getdate()) NOT NULL,
+    [DimUserID]        INT             IDENTITY (1, 1) NOT NULL,
+    [SourceKey]        INT             NOT NULL,
+    [DimDateOfBirthID] INT             NULL,
+    [FirstName]        VARCHAR (255)   NULL,
+    [MiddleName]       VARCHAR (255)   NULL,
+    [LastName]         VARCHAR (255)   NULL,
+    [Gender]           VARCHAR (50)    NULL,
+    [PhoneNumber]      VARBINARY (256) NULL,
+    [Email]            VARCHAR (100)   NULL,
+    [IsEnabled]        BIT             NULL,
+    [DimCreatedDateID] INT             NOT NULL,
+    [SourceKeyHash]    BIGINT          NULL,
+    [DeltaHash]        BIGINT          NULL,
+    [sys_ModifiedBy]   VARCHAR (255)   DEFAULT (suser_sname()) NOT NULL,
+    [sys_ModifiedOn]   DATETIME        DEFAULT (getdate()) NOT NULL,
+    [sys_CreatedBy]    VARCHAR (255)   DEFAULT (suser_sname()) NOT NULL,
+    [sys_CreatedOn]    DATETIME        DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [pk_DimUserID] PRIMARY KEY CLUSTERED ([DimUserID] ASC),
-    CONSTRAINT [fk_DimUser_CreatedDateID] FOREIGN KEY ([CreatedDateID]) REFERENCES [Shared].[DimDate] ([DimDateID]),
-    CONSTRAINT [fk_DimUser_DateOfBirthID] FOREIGN KEY ([DateOfBirthID]) REFERENCES [Shared].[DimDate] ([DimDateID])
+    CONSTRAINT [fk_DimUser_CreatedDateID] FOREIGN KEY ([DimCreatedDateID]) REFERENCES [Shared].[DimDate] ([DimDateID]),
+    CONSTRAINT [fk_DimUser_DateOfBirthID] FOREIGN KEY ([DimDateOfBirthID]) REFERENCES [Shared].[DimDate] ([DimDateID])
 );
+
+
 
 
 
