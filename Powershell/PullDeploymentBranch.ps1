e1ce97b1743589f8e44a37f02d6ec41d0f297b3b
christinaleo <christina.leo@aphelion.bi>
2015-02-05 10:02:22 +0100

    Testing Rescue Time commit messages

--- Powershell/PullDeploymentBranch.ps1
+++ Powershell/PullDeploymentBranch.ps1
@@ -1,10 +1,8 @@
-﻿Param (
-    [Parameter(Mandatory=$True)][String]$GitFolder
-      )
+﻿$GitFolder = "F:\Github\Pagatech"
 cd $GitFolder
 
-git fetch --all
-git reset --hard HEAD
-git clean -f -d
-git pull "origin" +refs/heads/R02_Joint/Deployment:refs/remotes/origin/R02_Joint/Deployment
-git checkout --force -B "R02_Joint/Deployment" "origin/R02_Joint/Deployment"
+git fetch --all 2>&1 | %{ "$_" }
+git reset --hard HEAD 2>&1 | %{ "$_" }
+git clean -f -d 2>&1 | %{ "$_" }
+git pull "origin" +refs/heads/R02_Joint/Deployment:refs/remotes/origin/R02_Joint/Deployment 2>&1 | %{ "$_" }
+git checkout --force -B "R02_Joint/Deployment" "origin/R02_Joint/Deployment" 2>&1 | %{ "$_" }
