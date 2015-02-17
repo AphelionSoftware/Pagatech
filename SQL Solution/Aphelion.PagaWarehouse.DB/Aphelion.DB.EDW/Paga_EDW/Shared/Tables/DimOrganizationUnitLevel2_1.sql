222da8482f886f26365613236ad94deb9a124ed5
Christina E. Leo <christina.leo@aphelion.bi>
2015-02-17 09:57:24 +0000

    OrganizationUnitLevel Source Queries Updated

--- SQL Solution/Aphelion.PagaWarehouse.DB/Aphelion.DB.EDW/Paga_EDW/Shared/Tables/DimOrganizationUnitLevel2_1.sql
+++ SQL Solution/Aphelion.PagaWarehouse.DB/Aphelion.DB.EDW/Paga_EDW/Shared/Tables/DimOrganizationUnitLevel2_1.sql
@@ -36,6 +36,8 @@
 
 
 
+
+
 GO
 CREATE UNIQUE NONCLUSTERED INDEX [ix_DimOrganizationUnitLevel2_SourceKey]
     ON [Shared].[DimOrganizationUnitLevel2]([SourceKey] ASC);
@@ -80,11 +82,39 @@ EXECUTE sp_addextendedproperty @name = N'KeyColumn', @value = N'OrganizationUnit
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'BaseQuery', @value = N'SET NOCOUNT ON; DECLARE @OrgLevel AS INT = 2; DECLARE @OrgUnit AS Table ( 	[SourceKey] [varchar](255) , 	[Name] [varchar](255) , 	[DimOrganizationSourceKey] [int], 	[DimOrganizationUnitTypeSourceKey] [varchar](50), 	[IdentificationNumber] [varchar](20)  );  
-WITH cte AS (  	SELECT  		o.OrganizationId, 		ou.OrganizationUnitId, 		UnitName = COALESCE(ou.Name, o.name), 		IdentificationNumber, 		ot.[Description], 		ParentOrganizationUnitId = ou.[OrganizationUnitId], 		1 AS OrgLevel 	FROM [dbo].Organization AS o 	INNER JOIN dbo.OrganizationUnit AS ou ON 		ou.OrganizationId = o.OrganizationId 	LEFT JOIN dbo.OrganizationUnitType AS ot ON 		ou.OrganizationUnitTypeId = ot.OrganizationUnitTypeId 	WHERE ou.ParentOrganizationUnitId IS NULL 	UNION ALL 	SELECT  		st.OrganizationId, 		ou1.OrganizationUnitId, 		UnitName = ou1.Name, 		ou1.IdentificationNumber, 		ot1.[Description], 		ou1.ParentOrganizationUnitId, 		st.OrgLevel + 1 AS OrgLevel 	FROM [dbo].OrganizationUnit AS ou1 	INNER JOIN cte AS ST ON  		ou1.ParentOrganizationUnitId = ST.OrganizationUnitId 	INNER JOIN dbo.OrganizationUnitType AS ot1 ON 		ou1.OrganizationUnitTypeId = ot1.OrganizationUnitTypeId 	WHERE ou1.ParentOrganizationUnitId IS NOT NULL )  	INSERT INTO @OrgUnit 	( 		SourceKey, 		Name, 		DimOrganizationSourceKey, 		DimOrganizationUnitTypeSourceKey, 		IdentificationNumber 	)  	
-SELECT 		 SourceKey = cte.OrganizationUnitId, 		Name = CONVERT(VARCHAR(255),cte.UnitName), 		DimOrganizationSourceKey = cte.OrganizationId, 		DimOrganizationTypeSourceKey = COALESCE(LTRIM(RTRIM(cte.[Description])), ''UNKNOWN''), 		IdentificationNumber 	
-FROM cte 	WHERE  		cte.OrgLevel = @OrgLevel 	 	 	SELECT  		ct.SYS_CHANGE_OPERATION, paga_change_log_id = ct.row_id, SYS_CHANGE_VERSION = ct.as_of_change_version, SourceKey, 		base_query.Name, 		DimOrganizationUnitLevel1SourceKey = base_query.DimOrganizationSourceKey, 		base_query.DimOrganizationUnitTypeSourceKey, 		base_query.IdentificationNumber 	FROM @OrgUnit AS base_query
+EXECUTE sp_addextendedproperty @name = N'BaseQuery', @value = N'SET NOCOUNT ON; 
+DECLARE @OrgLevel AS INT = 2; 
+DECLARE @OrgUnit AS Table 
+( 	
+	[SourceKey] [varchar](255) , 	
+	[Name] [varchar](255) , 	
+	[DimOrganizationSourceKey] [int], 	
+	[DimOrganizationUnitTypeSourceKey] INT, 	
+	[IdentificationNumber] [varchar](20)  
+);  
+WITH cte AS 
+(  	SELECT  		o.OrganizationId, 		ou.OrganizationUnitId, 		
+
+UnitName = COALESCE(ou.Name, o.name), 		IdentificationNumber, 		ot.OrganizationUnitTypeId, 		ParentOrganizationUnitId = ou.[OrganizationUnitId], 		1 AS OrgLevel 	
+FROM [dbo].Organization AS o 	INNER JOIN dbo.OrganizationUnit AS ou ON 		ou.OrganizationId = o.OrganizationId 	LEFT JOIN dbo.OrganizationUnitType AS ot ON 		
+ou.OrganizationUnitTypeId = ot.OrganizationUnitTypeId 	WHERE ou.ParentOrganizationUnitId IS NULL 	UNION ALL 	SELECT  		st.OrganizationId, 		ou1.OrganizationUnitId, 		
+UnitName = ou1.Name, 		ou1.IdentificationNumber, 		ot1.OrganizationUnitTypeId, 		ou1.ParentOrganizationUnitId, 		st.OrgLevel + 1 AS OrgLevel 	
+FROM [dbo].OrganizationUnit AS ou1 	INNER JOIN cte AS ST ON  		ou1.ParentOrganizationUnitId = ST.OrganizationUnitId 	INNER JOIN dbo.OrganizationUnitType AS ot1 ON 		
+ou1.OrganizationUnitTypeId = ot1.OrganizationUnitTypeId 	WHERE ou1.ParentOrganizationUnitId IS NOT NULL )  	
+INSERT INTO @OrgUnit 	( 		SourceKey, 		Name, 		DimOrganizationSourceKey, 		DimOrganizationUnitTypeSourceKey, 		IdentificationNumber 	)  	
+SELECT 		
+	ct.SYS_CHANGE_OPERATION, 
+	SYS_CHANGE_VERSION = ct.as_of_change_version,
+	paga_change_log_id = ct.row_id,
+SourceKey = cte.OrganizationUnitId, 		Name = CONVERT(VARCHAR(255),cte.UnitName), 		DimOrganizationSourceKey = cte.OrganizationId, 		
+DimOrganizationTypeSourceKey = COALESCE(((cte.OrganizationUnitTypeId)), 0), 		IdentificationNumber 	FROM cte 	WHERE  		cte.OrgLevel = @OrgLevel 	 	 	
+SELECT  		SourceKey, 		base_query.Name, 		base_query.DimOrganizationSourceKey, 		base_query.DimOrganizationUnitTypeSourceKey, 		base_query.IdentificationNumber
+		
+	
+	FROM @OrgUnit AS base_query
 ', @level0type = N'SCHEMA', @level0name = N'Shared', @level1type = N'TABLE', @level1name = N'DimOrganizationUnitLevel2';
+
+
 
 
 
