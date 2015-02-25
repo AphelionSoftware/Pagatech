01b5c3c779ab2689c810c744d03838499892593d
Christina E. Leo <christina.leo@aphelion.bi>
2015-02-25 12:36:04 +0000

    updated the processEvent view

--- SQL Solution/Aphelion.PagaWarehouse.DB/Aphelion.DB.EDW/Paga_EDW/OLAP/Views/Finance_FactFinancialTransaction.sql
+++ SQL Solution/Aphelion.PagaWarehouse.DB/Aphelion.DB.EDW/Paga_EDW/OLAP/Views/Finance_FactFinancialTransaction.sql
@@ -1,4 +1,5 @@
 ﻿
+
 	
 CREATE VIEW [OLAP].[Finance_FactFinancialTransaction] AS
 	(
@@ -6,5 +7,5 @@ CREATE VIEW [OLAP].[Finance_FactFinancialTransaction] AS
 			edw.FactFinancialTxID,edw.SourceKey,edw.DimEffectiveDateID,edw.DimEffectiveTimeID,edw.DimFinancialTxDateID,edw.DimFinancialTxTimeID,edw.DimFinancialTxTypeID,edw.DimUserID,edw.FactProcessEventID,edw.Reversed,edw.FactRelatedFinancialTxID,edw.FactOriginalFinancialTxID,edw.DimCurrencyID,edw.TextDescription,edw.FinancialTx_Amount,edw.ExchangeRate,edw.ExternalReferenceNumber,edw.FinancialTx_Fee,edw.ForeignCurrencyAmount,edw.ReferenceNumber,edw.ShortCode,edw.Cancelled,edw.IsIntegrationTx,edw.FactIntegrationTxID
 		FROM Finance.FactFinancialTransaction AS edw
 		WHERE edw.IsActive = 1
-		AND edw.DimFinancialTxDateID >= 20150101
+		--AND edw.DimFinancialTxDateID >= 20150101
 	);
